8f745676f718899b5194ac84ea3a6e2f2a3d1a6e
Gus Heptig <gusheptig@gmail.com>
2020-11-01 16:32:24 -0500

    adding brand logo effect on landing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import { Container, Button } from "react-bootstrap";
 // import Axios from "axios";
 
 import "./style.scss";
+import logo from "../components/Navbar/moviephood_logo.png";
 
 function Home(props) {
   const { isAuth } = useContext(AuthContext);
@@ -13,22 +14,39 @@ function Home(props) {
   const titleArr = title.split("");
 
   useEffect(() => {
-    anime.timeline({ loop: false }).add({
-      targets: ".welcomeText .letter",
-      translateX: [40, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeInExpo",
-      duration: 1200,
-      delay: (el, i) => 500 + 30 * i,
-    });
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: ".welcomeBrand",
+        translateX: [0, -30],
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1100,
+        delay: (el, i) => 200 + 30 * i,
+      })
+      .add({
+        targets: " .letter",
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeInExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i,
+      });
   });
 
   return (
     <header className="startHeader" id="pageTop">
       <Container className="d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-white welcomeText">
+          <img
+            alt=""
+            src={logo}
+            width="125"
+            height="125"
+            className="float-left bg-light welcomeBrand p-2"
+          />
+          <h1 className="mx-auto mt-5 text-white welcomeText float-left">
             {titleArr.map((letter, i) => (
               <span className="letter" key={i}>
                 {letter}
